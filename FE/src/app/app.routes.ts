5e8d2ad2b7ccc0c0569a25e6c71fbf7bad65f0b5
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-05-07 10:37:20 +0200

    router outlet added to layout

--- FE/src/app/app.routes.ts
+++ FE/src/app/app.routes.ts
@@ -6,5 +6,19 @@ export const routes: Routes = [
     pathMatch: 'full',
     loadComponent: () =>
       import('./layout/layout.component').then((c) => c.LayoutComponent),
+    children: [
+      {
+        path: '',
+        redirectTo: 'calendar',
+        pathMatch: 'full',
+      },
+      {
+        path: 'calendar',
+        loadComponent: () =>
+          import(
+            './modules/calendar/pages/main-calendar-view/main-calendar-view.component'
+          ).then((c) => c.MainCalendarViewComponent),
+      },
+    ],
   },
 ];
