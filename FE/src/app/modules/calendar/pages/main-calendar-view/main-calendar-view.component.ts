00ccd056ab3b0f8d6a23c73095b0b268533ab90e
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-05-07 10:27:43 +0200

    bootstrap setup

---
+++ FE/src/app/modules/calendar/pages/main-calendar-view/main-calendar-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-calendar-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-calendar-view.component.html',
+  styleUrl: './main-calendar-view.component.scss'
+})
+export class MainCalendarViewComponent {
+
+}
