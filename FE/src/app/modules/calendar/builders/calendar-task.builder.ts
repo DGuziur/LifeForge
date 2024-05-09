ce53f6bf4c9d1b7391b8098c0fdfee50fbf5d89a
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-05-09 15:22:57 +0200

    task form builder created

---
+++ FE/src/app/modules/calendar/builders/calendar-task.builder.ts
@@ -0,0 +1,24 @@
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+export class CalendarTaskBuilder {
+  static buildDay(day: number, month: number, year: number): FormGroup {
+    return new FormGroup({
+      day: new FormControl<number>(day),
+      month: new FormControl<number>(month),
+      year: new FormControl<number>(year),
+      tasks: new FormArray([]),
+      calories: new FormControl<number>(0),
+    });
+  }
+
+  static buildTask(): FormGroup {
+    return new FormGroup({
+      completeDate: new FormControl<Date | null>(null),
+      createDate: new FormControl<Date>(new Date()),
+      dueDate: new FormControl<Date | null>(null),
+      description: new FormControl<string>(''),
+      isDone: new FormControl<boolean>(false),
+      title: new FormControl<string>(''),
+    });
+  }
+}
